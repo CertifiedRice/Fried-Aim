ceadf53c6e35504982eb260fd0a9812184b0f5a6
BossFreind1234 <vincentredtube@gmail.com>
2022-06-16 23:23:03 -0400

    End game ui(WIP)

---
+++ Assets/Scripts/EndGameUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class EndGameUI : MonoBehaviour
+{
+    public Button MenuReturn;
+    
+    void Update()
+    {
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,9 +9,12 @@ public class Player : MonoBehaviour
     public static Player Instance;
 
     public float Score;
+    public float timeRemaining = 5f;
     
     // UI Variables 
     public Text ScoreText;
+    public Text TimeText;
+    public GameObject EndGameUi;
     
     void Awake()
     {
@@ -20,11 +23,27 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            EndGameUi.SetActive(true);
+            Pause();
+        }
+
+        TimeText.text = "Time Left: " + timeRemaining;
         ScoreText.text = "Score: " + Score;
     }
 
     public void AddScore()
     {
         Score++;
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
     }
 }
